e94c0be2d1a6b6565be7f9bf078212622105be58
Jorge Sauma <josauma@gmail.com>
2017-08-23 17:51:52 -0600

    Cached Inverse Matrix
    Programming Assignment Week 3
    Functions:
    MakeCacheMatrix (mat): set a function list to manage the cache
    cacheSolve (list): calculate the inverse of a matrix or retreive it from cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,89 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Title: R Cache Matrix
+## Author: Jorge Sauma
+## Course: R Programming
+##         Programming assignment, week 3
+## Date: 08-24-2017
+## Version 1
+##
+## Note: The following matrix will be used for testing purposes:
+##  
+## Matrix:
+##       [,1] [,2] [,3]
+##  [1,]    1    2    3
+##  [2,]    0    1    4
+##  [3,]    5    6    0
+##
+## Inverse:
+##       [,1] [,2] [,3]
+##  [1,]  -24   18    5
+##  [2,]   20  -15   -4
+##  [3,]   -5    4    1
 
-## Write a short comment describing this function
+## makeCacheMatrix: creates a list with functions used to set and get
+## a matrix, and setinverse, getinverse to set and get the inverse
+## of this matrix
+## Arguments: 
+##            mat= Matrix object
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mat = matrix()) {
+
+    ## Is easy to verify that mat is a matrix. If it isn't, return NULL
+    if (!is.matrix(mat)){
+      message("Argument is not a matrix")
+      return()
+    }
+  
+		# mi= matrix inverse
+	    mi <- NULL
+
+		# nm= new matrix
+		# Note: Set is not used anywhere. Left here for
+        # future use
+        set <- function(nm) {
+                mat <<- nm
+                mi <<- NULL
+        }
+
+        get <- function() mat
+
+		# extmi: external matrix inverse (calculated outside this function)
+        setinverse <- function(extmi) mi <<- extmi
+        getinverse <- function() mi
+
+		# This is the list that will be returned
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
+## cacheSolve: calculate the inverse of a matrix, 
+## or return the result if it was calculated before
+## Arguments: 
+##            cachedmat= List returned by MakeCacheMatrix 
 
-## Write a short comment describing this function
+cacheSolve <- function(cachedmat, ...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## At least verify that cachedmat is not null. If it is NULL show a message
+  if (is.null(cachedmat)) {
+    message("Argument is NULL")
+    return()
+  }
+  
+  
+    ## Return a matrix that is the inverse (mi) of 'mat'
+		mi <- cachedmat$getinverse()
+
+        if(!is.null(mi)) {
+                message("Inverse already calculated:")
+                return(mi)
+        }
+		
+		## Inverse was not cached
+		message("Calculating inverse: ")
+		
+        mat_temp <- cachedmat$get()
+        mi <- solve(mat_temp)
+        cachedmat$setinverse(mi) # Caching the result
+        mi
 }
